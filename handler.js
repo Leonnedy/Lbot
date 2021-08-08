34f1f414f595f9d2118dfdefd5175b1de749ee37
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-08-08 13:07:39 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -211,7 +211,7 @@ module.exports = {
             }
           } finally {
             // m.reply(util.format(_user)) 
-            if (m.limit) m.reply(+ m.limit + ' Limit terpakai')
+            if (m.limit) m.reply(+ m.limit + ' Will fan de la pija')
           }
     			break
   	  	}
