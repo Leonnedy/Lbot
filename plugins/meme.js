b83f005038468c6a487a1ab08720aa264ba15075
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-08-08 02:52:30 -0400

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,6 +1,6 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
-  let res = await fetch(global.API('https://es.memedroid.com/memes/detail/3359739/La-puta-madre-estoy-harto-de-encontrarme-esos-anuncios', '/meme'))
+  let res = await fetch(global.API('https://some-random-api.ml', '/meme'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.image) throw 'Err!'
